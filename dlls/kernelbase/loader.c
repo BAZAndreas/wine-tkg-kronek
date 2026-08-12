570576e1b5a7fe7214e3cf834d82d944562e8246
Kron4ek <kron4ek@gmail.com>
2026-08-12 09:21:36 +0300

    Update to 11.15

--- dlls/kernelbase/loader.c
+++ dlls/kernelbase/loader.c
@@ -23,14 +23,14 @@
 /*******************************************************************************/
 /* Modification and Enhancement Narrative                                      */
 /*                                                                             */
-/* Craig Schulstad - Horace, ND  USA (2 May, 2026)                             */
+/* Craig Schulstad - Horace, ND  USA (8 August, 2026)                          */
 /*                                                                             */
 /* This program has been revised to reactively acquire an MUI file reference   */
 /* to be used by the various resource fetch functions.  Without these code     */
 /* changes, no MUI reference was found and the calling program was falling     */
 /* back to the "exe" file for information.                                     */
 /*                                                                             */
-/* Version being enhanced:  11.8                                               */
+/* Version being enhanced:  11.15                                              */
 /*                                                                             */
 /* The following function calls were added:                                    */
 /*   get_mui (Attempts to locate and retrieve an MUI file)                     */
@@ -1091,7 +1091,6 @@ BOOL WINAPI DECLSPEC_HOTPATCH EnumResourceTypesExW( HMODULE module, ENUMRESTYPEP
     HeapFree( GetProcessHeap(), 0, type );
     return ret;
 }
-
 
 /* MUI Start */
 /***********************************************************************/
